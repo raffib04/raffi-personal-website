f78542569b3394881f8a214fe42dce48d6e1eac9
raffib04 <rafaelbiedron2026@u.northwestern.edu>
2023-08-07 22:18:44 -0500

    logo

--- src/App.js
+++ src/App.js
@@ -2,10 +2,11 @@ import "./index.css";
 
 function App() {
     return (
-        <div>
-            <header>
-                <p class='font-logo bg-headerBlack text-8xl'>RB</p>
-                <p class=''>Save</p>
+        <div class='bg-cream h-screen w-screen'>
+            <header class='bg-slate-300 text-center md:text-left'>
+                <div class='inline-block font-logo text-headerBlack p-6 text-6xl md:text-8xl transform-gpu hover:rotate-180 origin-center'>
+                    RB
+                </div>
             </header>
         </div>
     );
